e81ffc8ad3d2fa55b2755d40a6cd43f89161226f
suika-kou <chuihua.kong@fusions.co.jp>
2022-06-06 14:41:36 +0900

    build jar for fusions

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -24,7 +24,7 @@ public class Constants {
     /**
      * The build date is updated for each public release.
      */
-    public static final String BUILD_DATE = "2022-05-02";
+    public static final String BUILD_DATE = "2022-06-06";
 
     /**
      * The build date is updated for each public release.
